6f8d07359ff8e2883c0a017b30f8a8e6add0a7f3
krrsalmane <karroumsalmane@gmail.com>
2026-03-24 14:56:21 +0100

    add routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/', function () {
+    return response()->json(['message' => 'Hello world!']);
+});
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('jwt')->group(function () {
+    Route::get('/user', [AuthController::class, 'getUser']);
+    Route::put('/user', [AuthController::class, 'updateUser']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
