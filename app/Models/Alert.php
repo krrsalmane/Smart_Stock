b83d2324cd8c69f141b687e8f3ac678b9f961ef7
krrsalmane <karroumsalmane@gmail.com>
2026-03-27 11:24:12 +0100

    create model Alert and Category

---
+++ app/Models/Alert.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Alert extends Model
+{
+    protected $fillable = ['status', 'triggered_at', 'type', 'product_id'];
+
+    // An alert belongs to one specific product
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
