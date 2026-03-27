a57f4a4a32101b49c657d0a6d1b1dfcafc82c003
krrsalmane <karroumsalmane@gmail.com>
2026-03-27 11:30:58 +0100

    create warhouse model

---
+++ app/Models/Warehouse.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Warehouse extends Model
+{
+    protected $fillable = ['name', 'address'];
+
+    // According to your diagram, a warehouse "contains" products
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+    
+    // It also has a Magasinier who "runs" it
+    public function magasinier()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
