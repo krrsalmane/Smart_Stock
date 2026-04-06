9c6c168f3a0b1269c6235b9ba59303c7aa8cbd8b
krrsalmane <karroumsalmane@gmail.com>
2026-04-06 14:50:35 +0100

    add store method to MouvementController for creating new mouvements

--- app/Http/Controllers/MouvementController.php
+++ app/Http/Controllers/MouvementController.php
@@ -15,7 +15,29 @@ class MouvementController extends Controller
         return response()->json($mouvements);
     }
 
-   
+    // Create a new mouvement
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'type' => 'required|in:IN,OUT,ADJ',
+            'quantity' => 'required|integer|min:1',
+            'note' => 'sometimes|string|max:255',
+            'product_id' => 'required|exists:products,id',
+            'command_id' => 'sometimes|exists:commands,id',
+            'user_id' => 'required|exists:users,id',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $mouvement = Mouvement::create($request->all());
+
+        return response()->json([
+            'message' => 'Mouvement recorded successfully',
+            'mouvement' => $mouvement->load(['product', 'command', 'user'])
+        ], 201);
+    }
 
     
 }
