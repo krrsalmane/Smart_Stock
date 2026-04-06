483a3f42a121838b0639b38da2c36ee7714a037f
krrsalmane <karroumsalmane@gmail.com>
2026-04-06 14:12:16 +0100

    validate fileds to create warhouse

--- app/Http/Controllers/WarehouseController.php
+++ app/Http/Controllers/WarehouseController.php
@@ -15,7 +15,25 @@ class WarehouseController extends Controller
         return response()->json($warehouses);
     }
 
-   
+    // Create a new warehouse
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'address' => 'required|string|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $warehouse = Warehouse::create($request->all());
+
+        return response()->json([
+            'message' => 'Warehouse created successfully',
+            'warehouse' => $warehouse->load('products')
+        ], 201);
+    }
 
     // Get a specific warehouse
   
