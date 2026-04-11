a21f1e64cbd5b7137a87acfa50b151d1af9a1e9f
krrsalmane <karroumsalmane@gmail.com>
2026-04-11 15:32:38 +0100

    add Supplier model with relationships to products and commands

---
+++ app/Models/Supplier.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Supplier extends Model
+{
+    protected $fillable = ['name', 'email', 'phone', 'address'];
+
+    /**
+     * Get the products that this supplier provides.
+     * A supplier provides many products, and a product can be provided by many suppliers.
+     */
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class)
+                    ->withPivot('cost_price', 'lead_time')
+                    ->withTimestamps();
+    }
+
+    /**
+     * Get the commands sent to this supplier.
+     * A supplier receives many commands, and a command can be sent to many suppliers.
+     */
+    public function commands(): BelongsToMany
+    {
+        return $this->belongsToMany(Command::class)
+                    ->withPivot('quantity_ordered', 'order_date', 'expected_delivery')
+                    ->withTimestamps();
+    }
+}
