8ef66301182985056fcea2c7f5c51fd328143d9d
krrsalmane <karroumsalmane@gmail.com>
2026-04-11 16:35:42 +0100

    initialize SupplierController with namespace and imports

---
+++ app/Http/Controllers/SupplierController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Supplier;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class SupplierController extends Controller
+{
+    
+
+
+  
+
+  
+  
+   
+}
