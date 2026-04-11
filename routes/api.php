2f7ea60cae0836753cd2a66a6367b1f70ae7348c
krrsalmane <karroumsalmane@gmail.com>
2026-04-11 19:39:46 +0100

    add magasinier specific routes for category management

--- routes/api.php
+++ routes/api.php
@@ -54,7 +54,17 @@ Route::middleware('jwt')->group(function () {
     Route::post('/suppliers/{id}/commands', [SupplierController::class, 'attachCommand']);
     Route::delete('/suppliers/{id}/commands/{commandId}', [SupplierController::class, 'detachCommand']);
 
-  
+    /*
+    |-- Magasinier Specific Routes --|
+    */
+    Route::middleware('magasinier')->group(function () {
+        // Categories
+        Route::get('/categories', [CategoryController::class, 'index']);
+        Route::post('/categories', [CategoryController::class, 'store']);
+        Route::get('/categories/{id}', [CategoryController::class, 'show']);
+        Route::put('/categories/{id}', [CategoryController::class, 'update']);
+        Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+
         // Warehouses
         Route::get('/warehouses', [WarehouseController::class, 'index']);
         Route::post('/warehouses', [WarehouseController::class, 'store']);
