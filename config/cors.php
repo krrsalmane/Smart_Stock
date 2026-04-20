2b02fc187c946a5d5aaa808aaa10330d4f84a21d
krrsalmane <karroumsalmane@gmail.com>
2026-04-20 15:11:43 +0100

    fix: update CORS allowed_origins to include localhost:8000 for Blade templates

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:4200'],
+    'allowed_origins' => ['http://localhost:4200', 'http://localhost:8000', 'http://127.0.0.1:8000'],
 
     'allowed_origins_patterns' => [],
 
